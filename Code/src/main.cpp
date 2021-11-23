a7898f0ee0d8dfce77576a06c71048a21dd3c26b
Phil McDonald <phil.w.mcdonald@gmail.com>
2021-11-22 22:24:34 -0500

    various refinements

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -4,21 +4,30 @@
 #include <Arduino.h>
 #include "tpms_silencer.h"
 
-#define PIN_EN A0  // PA0, Pin 13, Arduino 10
-#define PIN_FSK A1 // PA1, Pin 12, Arduino 9
-#define PIN_ASK A2 // PA2, Pin 11, Arduino 8
-#define PIN_BUTTON A7 // PA7, Pin 6, PCINT7
-#define INTERRUPT_PIN PCINT7
+/*
+Each bit/symbol: 0/1 is 100us long. sent at a rate of 10kHz
+144 symbols sent = 14.4ms long packets (+ 300us warmup)
+Packets are sent 40ms apart, then we sleep.
+If PERIODIC_TX is defined then wakeup after 7s * wakeuplimit (which is potentially variable)
+BACKOFF will double the retransmit period each time until MAX_LIMIT.
+*/
 
-#define ENABLE_WDT // disable for tx on button press only
-#define BACKOFF // double transmit period everytime
+#define PIN_EN     A0  // PA0, Pin 13, Arduino 10
+#define PIN_FSK    A1 // PA1, Pin 12, Arduino 9
+#define PIN_ASK    A2 // PA2, Pin 11, Arduino 8
+#define PIN_BUTTON A7 // PA7, Pin 6, PCINT7
+#define INTERRUPT_PIN PCINT7 // interrupt for PA7
+
+#define PERIODIC_TX // periodic retransmissions. disable for button-press tx only
+//#define BACKOFF    // double transmit period everytime? disable to have a fixed period retrans.
 
 #define ENHIGH (bitSet(PORTA, 0))
 #define ENLOW (bitClear(PORTA, 0))
 
+// * unecessary hardware modification *
 // Tie ASK and FSK signals together to drive high stronger/faster?
 // cut ASK link between attiny and MICRF112.
-// bodge ASK to EN on MICRF112. and ASK/FSK together on the attiny
+// bodge ASK to EN on MICRF112. and ASK/FSK together on the attiny.
 //#define FSK_MOD
 
 #ifdef FSK_MOD
@@ -26,10 +35,10 @@
 #define ASKHIGH {}
 #define ASKLOW {}
 
-// reversed for some reason
-#define FSKHIGH (PORTA &= ~(0x6))
-#define FSKLOW (PORTA |= 0x6)
-#define FSKTOGGLE (PORTA = PORTA ^ _BV(0x6))
+// drive PA1 & PA2 together
+#define FSKLOW (PORTA &= ~(0x6))
+#define FSKHIGH (PORTA |= 0x6)
+#define FSKTOGGLE (PORTA = PORTA ^ (0x6))
 
 #else
 
@@ -42,21 +51,18 @@
 
 #endif
 
-#define PACKETSIZE 144
-
-#define PACKET_DELAY 65 // Milliseconds period for packet tx
+#define PACKET_DELAY   40 // Milliseconds inter-packet period
 #ifdef BACKOFF
-#define LIMIT_START   1 // (re-transmit intervals: 14s, 28s, 56s, ..., MAX_LIMIT/8)
-#define MAX_LIMIT     160 // 80 max limit = 10mins
+#define LIMIT_START     1 // (re-transmit intervals: 14s, 28s, 56s, ..., MAX_LIMIT/8)
+//#define MAX_LIMIT      80 // 80 max limit = 10mins.  
+#define MAX_LIMIT     160 // 160 max limit = 20 mins.
 #else
-#define LIMIT_START   8 // ~60s = 8*(7s) wakeups before transmit
+//#define LIMIT_START     8 // ~60s period = 8*(7s) wakeups before transmit
+#define LIMIT_START     120 // ~15mins period = 120*(7s) wakeups before transmit
 #endif
 
 /*
-Each symbol 0/1 is 100us long. sent at a rate of 10kHz
-144 symbols sent = 14.4ms long packets (+ 300us warmup)
-Each packet sent 65ms appart
-
+captured tpms data: with rtl_433 -vv ...
 time      : 2021-10-22 18:46:04
 model     : PMV-107J     type      : TPMS          id        : 07698722
 status    : 16           battery_ok: 1             counter   : 2             failed    : OK            pressure_kPa: 228.160     temperature_C: 13.000     mic       : CRC
@@ -77,11 +83,6 @@ status    : 16           battery_ok: 1             counter   : 2             fai
 pulse_demod_pcm(): PMV-107J (Toyota) TPMS
 bitbuffer:: Number of rows: 1 
 [00] {144} fc cc b4 ac b2 b3 55 52 b5 52 aa ab 33 35 34 b3 53 2c 
-13C = 53 = 0x35 = 0 0 1 1 0 1 0 1
-1111110 01100110010110100101011001011001010110011010101010101001010110101010100101010101010101011001100110011010100110100101100110101001100101100
-        0 0 0 0 0 1 0 1 0 1 1 0 0 1 0 0 1 1 0 0 0 1 1 1 1 1 1 0 1 1 0 1 1 1 1 0 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 1 1 0 0 1 0 1 0 0 0 1 1 0 0 0 1 0 0
-                 |       |       |       |       |       |       |       |       |       |       |       |       |       |       |       |       |
-                0       a       c       4       c       7       e       d       e       f       f       8       1       9       4       6        
 _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
 time      : 2021-10-22 18:46:14
 model     : PMV-107J     type      : TPMS          id        : 00a89722
@@ -89,31 +90,36 @@ status    : 8            battery_ok: 1             counter   : 1             fai
 pulse_demod_pcm(): PMV-107J (Toyota) TPMS
 bitbuffer:: Number of rows: 1 
 [00] {144} fc aa d2 d5 2b 4c ad 52 ad 52 ab 4b 32 d5 34 d5 2b 2c
-*/
-/* check diff manchester:
-apparently:
-          FC CD 55 55 4A B4 D4 B2 AD 52 AB 54 CD 34 AB 54 CD 2C
-"1 111110 01 10 011010101010101010101010 0101010110100110101001011001010101101010100101010101101010100110011010011010010101011010101001100110100101100";
-          0  0  0 0 1 1 1 1 1 1 1 1 1 1  0 1 1 1 0 1 0 0 1 1 0 1 0 0 1 1 1 0 1 1 1 0 1 1 1 1 0 1 1 1 0 0 0 0 1 0 0 1 0 1 1 1 0 1 1 1 0 0 0 0 1 0 1 0 0
 
 */
 
 
 // use the compact encoding reported by rtl_433 -vv  <-f 315M -R 110> 
-// Could also store the Diff Manchester decoded values.
-// Or generate whole packets on the fly!
-const unsigned char packets[4][18] = { { 0xfc, 0xb3, 0x4d, 0x2b, 0x2a, 0xb3, 0x52, 0xad, 0x4a, 0xad, 0x54, 0xab, 0x32, 0xca, 0xcb, 0x4d, 0x2c, 0xac},
-                              { 0xfc, 0xd5, 0x55, 0x4b, 0x2d, 0x4c, 0xad, 0x52, 0xb2, 0xad, 0x55, 0x4b, 0x33, 0x2a, 0xcb, 0x4c, 0xcd, 0x4c},
-                              { 0xfc, 0xcc, 0xb4, 0xac, 0xb2, 0xb3, 0x55, 0x52, 0xb5, 0x52, 0xaa, 0xab, 0x33, 0x35, 0x34, 0xb3, 0x53, 0x2c},
-                              { 0xfc, 0xaa, 0xd2, 0xd5, 0x2b, 0x4c, 0xad, 0x52, 0xad, 0x52, 0xab, 0x4b, 0x32, 0xd5, 0x34, 0xd5, 0x2b, 0x2c} };
-volatile unsigned char packet=0;
-volatile unsigned int currentBit;
+// Could also store the Differential-Manchester decoded values...
+// Or generate whole packets on the fly.
+#define NO_PACKETS      4
+#define PACKET_LEN     18
+#define PACKETSIZE    (PACKET_LEN*8) // 144 bits
+const unsigned char packets[NO_PACKETS][PACKET_LEN] = { 
+  // packet 0
+  { 0xfc, 0xb3, 0x4d, 0x2b, 0x2a, 0xb3, 0x52, 0xad, 0x4a, 0xad, 0x54, 0xab, 0x32, 0xca, 0xcb, 0x4d, 0x2c, 0xac},
+  // packet 1
+  { 0xfc, 0xd5, 0x55, 0x4b, 0x2d, 0x4c, 0xad, 0x52, 0xb2, 0xad, 0x55, 0x4b, 0x33, 0x2a, 0xcb, 0x4c, 0xcd, 0x4c},
+  // packet 2
+  { 0xfc, 0xcc, 0xb4, 0xac, 0xb2, 0xb3, 0x55, 0x52, 0xb5, 0x52, 0xaa, 0xab, 0x33, 0x35, 0x34, 0xb3, 0x53, 0x2c},
+  // packet 3
+  { 0xfc, 0xaa, 0xd2, 0xd5, 0x2b, 0x4c, 0xad, 0x52, 0xad, 0x52, 0xab, 0x4b, 0x32, 0xd5, 0x34, 0xd5, 0x2b, 0x2c} };
 
-volatile bool transmitting = false;
+volatile unsigned char packet=0; // which packet is being sent 0 - NO_PACKETS-1
+volatile unsigned int currentBit; // which bit in that packet is next
 
-volatile unsigned int wakeupCounter = 0;
-volatile unsigned int wakeuplimit = LIMIT_START;  // How many 8-second wakeups before transmit
+volatile bool transmitting = false; // indicates if transmitting
 
+volatile unsigned int wakeupCounter = 0; // how many 8s watchdog timeouts since last transmit
+volatile unsigned int wakeuplimit = LIMIT_START;  // How many 8-second wakeups before transmit again
+
+
+// setup functions
 
 // 10khz Interrupt for an 8MHz clock
 void setupInterrupt8()
@@ -174,6 +180,7 @@ void setup()
   ADCSRA = 0;
   power_spi_disable();
   power_usart0_disable();
+  power_usart1_disable(); //probably ok
   power_timer2_disable();
   power_twi_disable();
 
@@ -182,7 +189,7 @@ void setup()
   pinMode(PIN_ASK, OUTPUT);
   pinMode(PIN_BUTTON, INPUT_PULLUP);
   
-#ifdef ENABLE_WDT
+#ifdef PERIODIC_TX
   /* Clear the reset flags. */
   MCUSR = 0;
 
@@ -203,6 +210,8 @@ void setup()
   wakeupCounter = wakeuplimit; // force an immediate transmit
 }
 
+// runtime functions
+
 // 100 usec timer for bit tx
 ISR(TIMER1_COMPA_vect)
 {
@@ -215,19 +224,20 @@ ISR(TIMER1_COMPA_vect)
     }
 
     // read off bits in bigendian order
-    if ( ( packets[packet][currentBit/8] & (0x80>>(currentBit % 8)) ) ) 
-      FSKHIGH;
+    if ( ( packets[packet][currentBit/8] & ( 0x80 >> (currentBit % 8) ) ) )
+      FSKLOW; // low signal - higher freq
     else
-      FSKLOW;
+      FSKHIGH; // high signal - lower freq
+    
     currentBit++;
   }
 }
 
-#ifdef ENABLE_WDT
+#ifdef PERIODIC_TX
 // watchdog timer is setup by setupInterrupt8(). roughly 7-8s at 3v.
 ISR(WDT_vect)
 {
-  // ensure next watchdog uses the interrupt too
+  // reset WDIE to ensure next watchdog uses the interrupt too (dont reboot!)
   WDTCSR |= bit(WDIE);
   wakeupCounter++;
 }
@@ -237,7 +247,7 @@ ISR(WDT_vect)
 ISR(PCINT0_vect)
 {
   if( digitalRead(PIN_BUTTON) == LOW ) {
-    // button press. force wakeup and initial retransmit period
+    // button press. wakeup, transmit and set initial retransmit period
     wakeuplimit = LIMIT_START;
     wakeupCounter = wakeuplimit;
   //} else {
@@ -245,6 +255,7 @@ ISR(PCINT0_vect)
   }
 }
 
+// sleep until an WDT/button interrupt fires
 void sleepyTime()
 {
   // Just in case
@@ -257,40 +268,17 @@ void sleepyTime()
   sleep_disable();
 }
 
+// queue up and prepare to send a packet
 void sendPacket(unsigned int which)
 {
   transmitting = false; // just in case...
-  packet = which;
+  packet = min( which, NO_PACKETS-1 );
   currentBit = 0;
 
   enableTX();
 }
 
-void loop()
-{
-  if (wakeupCounter >= wakeuplimit)
-  {
-    for (int i=0;i<4;i++) {
-      sendPacket(i);
-      delay(PACKET_DELAY);
-    }
-
-    // reset to wait for next tx
-    wakeupCounter = 0;
-
-#ifdef BACKOFF
-    // double the wakeuplimit each time to back-off and save battery
-    wakeuplimit *= 2;
-#ifdef MAX_LIMIT
-    // up to this limit
-    wakeuplimit = min(wakeuplimit, MAX_LIMIT);
-#endif
-#endif
-  }
-
-  sleepyTime();
-}
-
+// warm-up transmitter and enable bit-period interrupt
 void enableTX()
 {
   FSKLOW;
@@ -302,10 +290,36 @@ void enableTX()
   power_timer1_enable();
 }
 
+// disable transmitter and disable bit-period interrupt
 void disableTX()
 {
   ENLOW;
   FSKLOW;
   transmitting = false;
   power_timer1_disable();
+}
+
+void loop()
+{
+  if (wakeupCounter >= wakeuplimit)
+  {
+    for (int i=0; i<NO_PACKETS; i++) {
+      sendPacket(i);
+      delay(PACKET_DELAY);
+    }
+
+    // reset to wait for next tx
+    wakeupCounter = 0;
+
+#ifdef BACKOFF
+    // double the wakeuplimit each time to back-off and save battery
+    wakeuplimit *= 2;
+#ifdef MAX_LIMIT
+    // but only up to this limit:
+    wakeuplimit = min(wakeuplimit, MAX_LIMIT);
+#endif
+#endif
+  }
+
+  sleepyTime();
 }
